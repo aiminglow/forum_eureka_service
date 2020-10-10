a2a2a1ad162f00e6c7c305be29a580fd3ddcbe07
aiminglow <zhangchichichichi@gmail.com>
2020-10-10 20:56:15 +0800

    eureka service init

---
+++ src/test/java/com/aiming/low/forum_eureka_service/ForumEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiming.low.forum_eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
